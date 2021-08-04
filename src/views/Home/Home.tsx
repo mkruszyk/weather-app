496c1a219cf515c793b5310dc8c25d8b1bfed00f
Michał Kruszyk <michal.kruszyk@monterail.com>
2021-08-04 17:26:27 +0200

    (chore): Basic setup

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Weather App</title>
+      </Head>
+      <main>
+        <h1 className="font-sans">Weather App</h1>
+      </main>
+    </>
+  );
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: ["./src/views/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
